74b87bef2393ebb2003d7198de0b597e593d6d69
Rashmi V Abbigeri <rashmiabbigeri@gmail.com>
2022-10-26 10:44:34 +0530

    display person info

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -7,7 +7,7 @@ import User from "../models/Users";
 import { useRouter } from "next/router";
 import { Box } from "@chakra-ui/react";
 
-function Dashboard({ username, orcid }) {
+function Dashboard({ username, orcid, records }) {
   const router = useRouter();
 
   const logout = () => {
@@ -22,6 +22,12 @@ function Dashboard({ username, orcid }) {
       </Box>
       <div>Welcome {username}!</div>
       <div>{orcid}</div>
+      Given name:
+      {records["name"]["given-names"]["value"]}
+      {"  "}
+      Family name:
+      {records["name"]["family-name"]["value"]}
+      {"  "}
       <button onClick={logout}>Logout</button>
     </div>
   );
@@ -33,12 +39,27 @@ export async function getServerSideProps({ req, res }) {
     await connect();
     // check cookie
     const token = getCookie("token", { req, res });
-    if (!token){
-      console.error("token missing, login again")
-      };
+    if (!token) {
+      console.error("token missing, login again");
+    }
 
     const verified = await jwt.verify(token, process.env.JWT_SECRET);
     const obj = await User.findOne({ _id: verified.id });
+
+    const URL = `https://api.sandbox.orcid.org/v3.0/${obj.orcid}/person`;
+    const bearerToken = "Bearer" + obj.accessToken;
+    const resp = await fetch(URL, {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        Authorization: bearerToken,
+        "Content-Type": "application/vnd.orcid+json",
+      },
+    });
+
+    const userRecords = await resp.json();
+    console.log(userRecords);
+
     if (!obj)
       return {
         redirect: {
@@ -49,9 +70,11 @@ export async function getServerSideProps({ req, res }) {
       props: {
         orcid: obj.orcid,
         username: obj.username,
+        records: userRecords,
       },
     };
   } catch (err) {
+    console.error(err);
     removeCookies("token", { req, res });
     return {
       redirect: {
